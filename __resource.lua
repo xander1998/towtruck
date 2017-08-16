74060f20129cddbb8c11e3a1b659a4ee1d29e57d
xander1998 <xander.harrison77@gmail.com>
2017-08-16 15:44:04 -0500

    towtruck script

---
+++ __resource.lua
@@ -0,0 +1,6 @@
+-- Scripted by Xander Tanner-Harrison || For LESALife --
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'cl_towtruck.lua'
+server_script 'sv_towtruck.lua'
